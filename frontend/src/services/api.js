e8a84e198bd607f294e4d45136d5343e6c4b5930
ANJUSRI2607 <koduruanjusri@gmail.com>
2026-07-19 20:35:34 +0530

    Updated frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://hospital-backend-72gn.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
